9bbd4313187c206fd10524e1859a431e1c195382
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-04-25 05:15:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,21 +14,17 @@ def batch_badge_creator(attendees)
 end
 
 
-# def assign_rooms
+def assign_rooms(room)
+  cuartos = []
+  counter = 1
+  room.each do |name|
+  cuartos << "Hello, #{name}! You'll be assignet to room #{room}!"
+   counter += 1
+  end
+  cuartos
+end
 
 
-
-# def badge_maker(name)
-#     "Hello, my name is #{name}."
-# end
-
-# def batch_badge_creator(attendees)
-#   badges = []
-#   attendees.each do |name|
-#     badges << badge_maker(name)
-#   end
-#   badges
-# end
 
 # def assign_rooms(room)
 #   cuartos = []
